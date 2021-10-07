9e4c28ee4d422f87768715dbae26b3523150d1e4
jose.reyes <jose.reyes@missionlane.com>
2021-10-07 10:52:24 -0500

    TORCA-500 Fixing configuration issues.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,9 @@ repositories {
     mavenCentral()
 }
 
-extra["springBootAdminVersion"] = "2.3.0"
+extra["springBootAdminVersion"] = "2.4.3"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("de.codecentric:spring-boot-admin-starter-client")
     implementation("de.codecentric:spring-boot-admin-starter-server")
@@ -26,7 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("de.codecentric:spring-boot-admin-starter-server:2.3.0")
     implementation("de.codecentric:spring-boot-admin-server-ui:2.3.0")
-    implementation("org.springframework.boot:spring-boot-starter-security:2.5.0")
+    implementation("org.springframework.boot:spring-boot-starter-security:2.4.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
